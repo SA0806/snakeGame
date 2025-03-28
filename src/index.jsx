9e97a47630abd56e2e055e95b786edada66888c2
lobochironji <tanishq52212@gmail.com>
2025-03-28 20:39:51 +0530

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import './index.css';
+import App from './App.jsx';
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  <App/>
+  </StrictMode>
+)
